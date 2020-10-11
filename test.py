7f11689b12a7ab583a4279db47330b883faf8f8b
Eugene Li <lieugenebaba01@gmail.com>
2020-10-11 04:36:38 -0400

    fixed api

--- test.py
+++ test.py
@@ -5,10 +5,10 @@ local_url = 'http://localhost:5000/predict'
 heroku_url = 'https://speciate.herokuapp.com/predict'
 floydhub_url = 'https://www.floydlabs.com/serve/HEymaCcSNwJrkYHyCQHy3Q/predict'
 
-path = './data/image-0.jpg'
+path = './data/image-1.jpg'
 file = {'file': open(path, 'rb')}
 
-response = requests.post(local_url, file)
+response = requests.post(local_url, files=file)
 print(response)
 if (response.status_code == 200):
 	print(response.json())
